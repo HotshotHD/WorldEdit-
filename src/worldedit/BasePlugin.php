ffe9f32778d5cb9f1bec70423bc701011205c2df
Kareem <hshot9930@gmail.com>
2016-05-25 15:41:44 -0400

    Update BasePlugin.php

--- src/worldedit/BasePlugin.php
+++ src/worldedit/BasePlugin.php
@@ -1,1 +1,27 @@
+<?php
 
+namespace worldedit;
+
+use pocketmine\plugin\PluginBase;
+use worldedit\utils\Utils;
+use worldedit\commands\Commands;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+
+class BasePlugin extends PluginBase {
+	
+	public function onEnable() {
+		$this->getServer()->getPluginManager()->registerEvents(new EventListener($this), $this);
+	}
+	
+	public function onCommand(CommandSender $sender, Command $cmd, $label, array $args) {
+	    $cmds = new Commands($this);
+		
+		$cmds->onCommand($sender,$cmd, $label,$args);
+	}
+	
+	
+	public function getUtils() {
+		return new Utils();
+	}
+}
