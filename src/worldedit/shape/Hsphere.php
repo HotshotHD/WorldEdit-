bf3f1d1c7cdff093cf37fbda71a88a6e03d258d2
Kareem <hshot9930@gmail.com>
2016-05-25 15:43:49 -0400

    Create Hsphere.php

---
+++ src/worldedit/shape/Hsphere.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace worldedit\shape;
+
+use pocketmine\math\Vector3;
+use pocketmine\level\Level;
+
+class Hsphere {
+	
+	private $center;
+	
+	private $level;
+	
+	private $size;
+	
+	public function __construct(Vector3 $center, Level $level, int $size) {
+		$this->center = $center;
+		$this->level = $level;
+		$this->size = $size;
+	}
+}
